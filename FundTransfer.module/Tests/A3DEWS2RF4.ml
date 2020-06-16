d805ecbe6e58b780ae1ea2f5157c1c9510731b76
Ganitagya <akash.mhptr@gmail.com>
2020-06-16 15:03:07 +0530

    commit message

---
+++ FundTransfer.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <EmulationDataFile>ACED000574001846756E645472616E736665722E6D6F64756C65312E627774</EmulationDataFile>
+</emulation:BWTFile>
